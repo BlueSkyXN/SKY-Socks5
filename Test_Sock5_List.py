0050be1e6716169a3c5328c961a6a967360ed50b
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-08-02 10:35:51 +0800

    0.1.7

--- Test_Sock5_List.py
+++ Test_Sock5_List.py
@@ -3,9 +3,9 @@ import asyncio
 from aiohttp_socks import ProxyConnector, ProxyType
 
 # 并发量控制
-CONCURRENT_TASKS = 100  # 并发任务数量
+CONCURRENT_TASKS = 300  # 并发任务数量
 # 超时时间设置
-TIMEOUT_SECONDS = 15  # 超时时间，单位是秒
+TIMEOUT_SECONDS = 10  # 超时时间，单位是秒
 
 # 测试单个代理
 async def test_proxy(session, proxy, semaphore):
